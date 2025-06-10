b48c835341c6d5b48957765e071b56ecb345d3b3
stereno <160313150+stereno@users.noreply.github.com>
2025-06-10 23:59:11 +0900

    change: dev

--- dev.nix
+++ dev.nix
@@ -1,6 +1,22 @@
 { config, pkgs, ... }:
 
 {
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "stereno";
+  home.homeDirectory = "/home/stereno";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "25.05"; # Please read the comment before changing.
+
+  nixpkgs.config.allowUnfree = true;
+
   # Development-specific packages
   home.packages = [
     pkgs.nix-direnv
@@ -12,7 +28,6 @@
     direnv = {
         enable = true;
         enableBashIntegration = true;
-        nix-direnv.enable = true;
     };
     git = {
         enable = true;
